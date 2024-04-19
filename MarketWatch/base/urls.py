3ef879484a5046f38699bb6cafcf9286fab2e273
ssomo <sofia.mohamed.733@gmail.com>
2024-04-18 21:08:00 -0400

    Edited the updating information function for the Information and Profile pages

--- MarketWatch/base/urls.py
+++ MarketWatch/base/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     path('profile/', views.profile, name="profile"),
     path('edit_profile/', views.edit_profile, name="edit_profile"),
     path('information/', views.information, name="information"),
+    path('edit_information/', views.editInformation, name='edit_information'),
     path('loan_calculator/', views.loanCalculator, name='loan_calculator'),
     path('login/', views.loginUser, name="login"),
     path('signup/', views.signup, name="signup"),
